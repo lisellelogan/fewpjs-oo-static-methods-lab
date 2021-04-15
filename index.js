bde3d2522acde0ab5799f044a393683a715f9052
lisellelogan <liselle.logan@yahoo.com>
2021-04-15 16:26:10 -0400

    commit tree d9ef00b4de5aaea7223e1aecfd44f74c590f4ff1

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 class Formatter {
-  //add static methods here
+  static capitalize(word){
+    return word.charAt(0).toUpperCase() + word.slice(1);
+  }
+
+  static sanitize(word){
+    return word.replace(/[^A-Za-z0-9 '-]/g, '')
+  }
+
+  static titleize(sentence){
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const arrOfWords = sentence.split(' ');
+    const capitalizeWord = this.capitalize;
+
+    return arrOfWords.map(word => {
+      if (arrOfWords[0] === word){
+        return capitalizeWord(word)
+      } else if (exceptions.includes(word)){
+        return word
+      } else {
+        return capitalizeWord(word)
+      }
+    }).join(' ')
+  }
+
+    
 }
